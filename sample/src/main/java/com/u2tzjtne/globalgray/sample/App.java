b3097183dca964dc6e4c7a0b242b77de7eacaeb1
U2tzJTNE <u2tzjtne@gmail.com>
2020-04-16 20:50:52 +0800

    Change:延迟重启app

--- sample/src/main/java/com/u2tzjtne/globalgray/sample/App.java
+++ sample/src/main/java/com/u2tzjtne/globalgray/sample/App.java
@@ -2,6 +2,7 @@ package com.u2tzjtne.globalgray.sample;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
 
 import com.u2tzjtne.globalgray.GlobalGray;
 
@@ -10,11 +11,13 @@ import com.u2tzjtne.globalgray.GlobalGray;
  */
 public class App extends Application {
     private static Context context;
+    private static final String TAG = "App";
 
     @Override
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
+        Log.d(TAG,"app is onCreate: enable_gray "+SPUtils.getBoolean("enable_gray", false));
         GlobalGray.enable(SPUtils.getBoolean("enable_gray", false));
     }
 
--- sample/src/main/java/com/u2tzjtne/globalgray/sample/MainActivity.java
+++ sample/src/main/java/com/u2tzjtne/globalgray/sample/MainActivity.java
@@ -1,12 +1,17 @@
 package com.u2tzjtne.globalgray.sample;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 
 import java.util.List;
@@ -27,16 +32,45 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         mSwitchCompat = findViewById(R.id.sw_enable_gray);
+        Button mBtnShowDialog = findViewById(R.id.btn_show_dialog);
 
-        mSwitchCompat.setChecked(SPUtils.getBoolean("enable_gray",false));
+        mBtnShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
+
+        mSwitchCompat.setChecked(SPUtils.getBoolean("enable_gray", false));
 
         mSwitchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                SPUtils.putBoolean("enable_gray",mSwitchCompat.isChecked());
-                restartApp();
+                SPUtils.putBoolean("enable_gray", mSwitchCompat.isChecked());
+                //需要延迟启动，否则可能还没有写入SP应用进程就被杀死了
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        restartApp();
+                    }
+                },300);
             }
         });
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.app_name)
+                .setIcon(R.mipmap.ic_launcher)
+                .setMessage("this is a dialog!")
+                .setNegativeButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setCancelable(true)
+                .show();
     }
 
     private void restartApp() {
